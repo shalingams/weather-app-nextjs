28159123f1760ec47145e240071c4fc5e54d1a8e
Shalinga Manasinghe <shalingams@gmail.com>
2024-04-26 12:40:41 +0300

    Navbar setup

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { MdWbSunny, MdMyLocation, MdOutlineLocationOn } from "react-icons/md";
+import SearchBox from './SearchBox';
 
 type Props = {}
 
 export default function Navbar({}: Props) {
   return (
-    <div className='shadow-sa sticky top-0 left-0 z-50 bg-white'>Navbar</div>
-  )
+    <div className="shadow-sa sticky top-0 left-0 z-50 bg-white">
+      <div className="h-[80px] w-full flex justify-between items-center max-w-7x1 px-3 mx-auto">
+        <div className="flex items-center justify-center gap-2">
+          <h2 className="text-gray-500 text-3xl">Weather</h2>
+          <MdWbSunny className="text-yellow-300 text-3xl mt-1" />
+        </div>
+        <section className='flex gap-2 items-center'>
+          <MdMyLocation className='text-2xl text-gray-400 hover:opacity-80 cursor-pointer' />
+          <MdOutlineLocationOn className='text-3xl' />
+          <p className='text-slate-900/80 text-sm'>Tammela</p>
+          <div><SearchBox value={''} onChange={undefined} onSubmit={undefined} /></div>
+        </section>
+
+      </div>
+    </div>
+  );
 }
